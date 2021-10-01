799039126d72b361e1b2425f518f271cf1e8b6dd
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-10-01 22:59:30 +0300

    Update main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Romanov on 01.10.2021.
+//
+#include "main.h"
+#include "sha256.h"
+#include <iostream>
+
+int main() {
+    SHA256 sha256;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Romanov on 01.10.2021.
+//
+
+#ifndef ATM_GIT_MAIN_H
+#define ATM_GIT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ATM_GIT_MAIN_H
