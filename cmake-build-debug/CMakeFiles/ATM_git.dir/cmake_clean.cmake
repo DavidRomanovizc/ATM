65b1bcb908144e9d9c7ee6b63e0842810a39a68b
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-10-01 23:00:59 +0300

    Update

---
+++ cmake-build-debug/CMakeFiles/ATM_git.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ATM_git.exe"
+  "ATM_git.exe.manifest"
+  "ATM_git.pdb"
+  "CMakeFiles/ATM_git.dir/main.cpp.obj"
+  "CMakeFiles/ATM_git.dir/src/sha256.cpp.obj"
+  "libATM_git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATM_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
