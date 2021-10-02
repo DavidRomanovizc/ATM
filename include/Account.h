7b94d8355ff7dffab62c48b3eaf7672928b030d8
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-10-02 13:06:17 +0300

    Create user Account

---
+++ include/Account.h
@@ -0,0 +1,8 @@
+//
+// Created by Romanov on 02.10.2021.
+//
+
+#ifndef ATM_GIT_ACCOUNT_H
+#define ATM_GIT_ACCOUNT_H
+
+#endif //ATM_GIT_ACCOUNT_H
---
+++ src/Account.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Romanov on 02.10.2021.
+//
+
