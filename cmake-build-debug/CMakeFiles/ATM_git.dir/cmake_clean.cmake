07d770fe5d96b7da27da1c66d91ff91135b94ba9
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-10-03 19:39:47 +0300

    Update CMake

--- cmake-build-debug/CMakeFiles/ATM_git.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/ATM_git.dir/cmake_clean.cmake
@@ -4,6 +4,7 @@ file(REMOVE_RECURSE
   "ATM_git.pdb"
   "CMakeFiles/ATM_git.dir/main.cpp.obj"
   "CMakeFiles/ATM_git.dir/src/Account.cpp.obj"
+  "CMakeFiles/ATM_git.dir/src/Bank.cpp.obj"
   "CMakeFiles/ATM_git.dir/src/Menu.cpp.obj"
   "CMakeFiles/ATM_git.dir/src/sha256.cpp.obj"
   "libATM_git.dll.a"
