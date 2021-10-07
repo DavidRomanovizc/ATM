f105b6d91b3ed005c5937ed5dd1d5267f1b7c06e
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-10-07 17:51:52 +0300

    Update

--- include/States.h
+++ include/States.h
@@ -5,4 +5,35 @@
 #ifndef ATM_GIT_STATES_H
 #define ATM_GIT_STATES_H
 
+#include <iostream>
+
+class Statement {
+private:
+    double m_deposit, m_withdraw, m_balance;
+public:
+    Statement();
+
+    Statement(double deposit, double withdraw, double balance);
+
+    double getDeposit();
+
+    void setDeposit(double deposit);
+
+    double getWithdraw();
+
+    void setWithdraw(double withdraw);
+
+    double getBalance();
+
+    void setBalance(double balance);
+
+    void inputDeposit();
+
+    void inputWithdraw();
+
+    void showBalance();
+
+    void displayData();
+};
+
 #endif //ATM_GIT_STATES_H
--- src/States.cpp
+++ src/States.cpp
@@ -2,3 +2,61 @@
 // Created by Romanov on 07.10.2021.
 //
 
+#include <States.h>
+
+Statement::Statement(double deposit, double withdraw, double balance)
+        : m_deposit(deposit),
+          m_balance(balance),
+          m_withdraw(withdraw) {
+
+}
+
+Statement::Statement()
+        : m_deposit(0),
+          m_withdraw(0),
+          m_balance(0) {
+
+}
+
+double Statement::getDeposit() {
+    return m_deposit;
+}
+
+void Statement::setDeposit(double deposit) {
+    m_deposit = deposit;
+}
+
+double Statement::getWithdraw() {
+    return m_withdraw;
+}
+
+void Statement::setWithdraw(double withdraw) {
+    m_withdraw = withdraw;
+}
+
+double Statement::getBalance() {
+    return m_balance;
+}
+
+void Statement::setBalance(double balance) {
+    m_balance = balance;
+}
+
+void Statement::inputDeposit() {
+    std::cout << "ENTER AMOUNT TO DEPOSIT: ";
+    std::cin >> m_deposit;
+}
+
+void Statement::inputWithdraw() {
+    std::cout << "ENTER AMOUNT TO WITHDRAW: ";
+    std::cin >> m_withdraw;
+
+}
+
+void Statement::showBalance() {
+    std::cout << "CURRENT BALANCE: " << m_balance << std::endl;
+}
+
+void Statement::displayData() {
+    std::cout << m_deposit << "\t\t" << m_withdraw << "\t\t\t" << m_balance << std::endl;
+}
